128d57f9388a9d9c967acc7f37d3161576f01b0b
Joshua Wöhle <jwohle@gmail.com>
2013-03-14 13:20:17 +0000

    Initial clean-up and started to migrate network connections to background tasks

--- src/moodle/android/moodle/CourseAssignmentView.java
+++ src/moodle/android/moodle/CourseAssignmentView.java
@@ -1,22 +1,22 @@
 /**
-*  an Android implementation of REST and XML-RPC access to Moodle 2.2 servers or higher
-*  Copyright (C) 2012  Justin Stevanz, Andrew Kelson and Matthias Peitsch
-*
-*	Contact the.omega.online@gmail.com for further information.
-*
-*   This program is free software: you can redistribute it and/or modify
-*    it under the terms of the GNU General Public License as published by
-*    the Free Software Foundation, either version 3 of the License, or
-*    (at your option) any later version.
-*
-*    This program is distributed in the hope that it will be useful,
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*    GNU General Public License for more details.
-*
-*    You should have received a copy of the GNU General Public License
-*    along with this program.  If not, see <http://www.gnu.org/licenses/>
-*/
+ *  an Android implementation of REST and XML-RPC access to Moodle 2.2 servers or higher
+ *  Copyright (C) 2012  Justin Stevanz, Andrew Kelson and Matthias Peitsch
+ *
+ *	Contact the.omega.online@gmail.com for further information.
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
 
 package moodle.android.moodle;
 
@@ -49,167 +49,172 @@ import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 
 public class CourseAssignmentView extends Activity implements OnClickListener {
-		
-		Button home, courseSelect, upload, setting;
-		TextView footerCourseHdr;
-		LinearLayout emptyLayout;
-		FrameLayout courseworkLayout;
-		User user;
-		
-		SectionListItem[] assignArray;
-		StandardArrayAdapter arrayAdapter;
-		SectionListAdapter sectionAdapter;
-		SectionListView listView;
-		
-	    /** Called when the activity is first created. */
-	    @Override
-	    public void onCreate(Bundle savedInstanceState) {
-	    
-	        super.onCreate(savedInstanceState);
-	        setContentView(R.layout.course_assignment);  
-	        
-	        try
-	        {
-	        	Intent i = getIntent();	            
-	        	user = (User) i.getParcelableExtra("userObject");
-	        	
-	        	footerCourseHdr = (TextView) findViewById(R.id.course_ftr_view);
-	        	
-	        	home = (Button) findViewById(R.id.coursework_home_view);
-		        courseSelect = (Button) findViewById(R.id.select_course);
-				setting = (Button) findViewById(R.id.settings_view);
-		        upload = (Button) findViewById(R.id.upload_view);	        	
-	        	
-	        	if (user != null && user.getCourses().size() == 1) {
-	        		user.setSelectedCourseId(user.getCourses().get(0).getId());
-	        		courseSelect.setEnabled(false);
-	        	}        		
-	        	else
-	        		courseSelect.setEnabled(true);
-	        	
-	        	if (user != null && user.getSelectedCourseId() == 99999) {
-	        		i = new Intent(this, CourseSelect.class);
-	        		i.putExtra("userObject", user);
-	        		startActivityForResult(i, COURSE_SELECT_REQUEST_CODE);
-	        	}
-	        	
-	        	if (user != null && user.getSelectedCourseId() != 99999)
-	        		footerCourseHdr.setText(user.getCourse(user.getSelectedCourseId()).getShortName());
-	        	
-	        	getCourseAssignments();
-		        
-		        home.setOnClickListener(this);
-				if (courseSelect.isEnabled())
-					courseSelect.setOnClickListener(this);
-		        setting.setOnClickListener(this);
-		        upload.setOnClickListener(this);
-	        }
-	        catch(Exception e)
-	        {
-	        	Log.e("Error 1", e.toString()+"Error within CourseAssignmentView Class");
-	        }
-	        
-	    }
-	    
-	    public static final int COURSE_SELECT_REQUEST_CODE = 1;
 
-		public void onClick(View v) {
-			Intent nextPage;
+	Button home, courseSelect, upload, setting;
+	TextView footerCourseHdr;
+	LinearLayout emptyLayout;
+	FrameLayout courseworkLayout;
+	User user;
 
-			switch(v.getId())
-			{	
-			case R.id.coursework_home_view:
-				nextPage = new Intent(this, CourseDetail.class);
-				nextPage.putExtra("userObject", user);		
-				startActivity(nextPage);
-				break;
-			case R.id.select_course:
-				nextPage = new Intent(this, CourseSelect.class);
-				nextPage.putExtra("userObject", user);
-				startActivityForResult(nextPage, COURSE_SELECT_REQUEST_CODE);
-				break;
-			case R.id.settings_view:
-				nextPage = new Intent(this, Setting.class);
-				startActivity(nextPage);
-				break;
-			case R.id.upload_view:
-				nextPage = new Intent(this, FileUpload.class);
-				nextPage.putExtra("userObject", user);
-				startActivity(nextPage);
-				break;
-				default:
-					
+	SectionListItem[] assignArray;
+	StandardArrayAdapter arrayAdapter;
+	SectionListAdapter sectionAdapter;
+	SectionListView listView;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.course_assignment);
+
+		try {
+			Intent i = getIntent();
+			user = (User) i.getParcelableExtra("userObject");
+
+			footerCourseHdr = (TextView) findViewById(R.id.course_ftr_view);
+
+			home = (Button) findViewById(R.id.coursework_home_view);
+			courseSelect = (Button) findViewById(R.id.select_course);
+			setting = (Button) findViewById(R.id.settings_view);
+			upload = (Button) findViewById(R.id.upload_view);
+
+			if (user != null && user.getCourses().size() == 1) {
+				user.setSelectedCourseId(user.getCourses().get(0).getId());
+				courseSelect.setEnabled(false);
+			} else
+				courseSelect.setEnabled(true);
+
+			if (user != null && user.getSelectedCourseId() == 99999) {
+				i = new Intent(this, CourseSelect.class);
+				i.putExtra("userObject", user);
+				startActivityForResult(i, COURSE_SELECT_REQUEST_CODE);
 			}
+
+			if (user != null && user.getSelectedCourseId() != 99999)
+				footerCourseHdr.setText(user.getCourse(
+						user.getSelectedCourseId()).getShortName());
+
+			getCourseAssignments();
+
+			home.setOnClickListener(this);
+			if (courseSelect.isEnabled())
+				courseSelect.setOnClickListener(this);
+			setting.setOnClickListener(this);
+			upload.setOnClickListener(this);
+		} catch (Exception e) {
+			Log.e("Error 1", e.toString()
+					+ "Error within CourseAssignmentView Class");
 		}
 
-		protected void onActivityResult(int requestCode, int resultCode, Intent data) 
-		{
-			if (requestCode == COURSE_SELECT_REQUEST_CODE) {
-				if (resultCode == RESULT_OK) {
-					user = (User) data.getParcelableExtra("userObject");
-					if (user != null && user.getSelectedCourseId() != 99999) {
-						Course course = user.getCourse(user.getSelectedCourseId());
-						footerCourseHdr.setText(course.getShortName());
-						
-						getCourseAssignments();
-					}
+	}
+
+	public static final int COURSE_SELECT_REQUEST_CODE = 1;
+
+	public void onClick(View v) {
+		Intent nextPage;
+
+		switch (v.getId()) {
+		case R.id.coursework_home_view:
+			nextPage = new Intent(this, CourseDetail.class);
+			nextPage.putExtra("userObject", user);
+			startActivity(nextPage);
+			break;
+		case R.id.select_course:
+			nextPage = new Intent(this, CourseSelect.class);
+			nextPage.putExtra("userObject", user);
+			startActivityForResult(nextPage, COURSE_SELECT_REQUEST_CODE);
+			break;
+		case R.id.settings_view:
+			nextPage = new Intent(this, Setting.class);
+			startActivity(nextPage);
+			break;
+		case R.id.upload_view:
+			nextPage = new Intent(this, FileUpload.class);
+			nextPage.putExtra("userObject", user);
+			startActivity(nextPage);
+			break;
+		default:
+
+		}
+	}
+
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == COURSE_SELECT_REQUEST_CODE) {
+			if (resultCode == RESULT_OK) {
+				user = (User) data.getParcelableExtra("userObject");
+				if (user != null && user.getSelectedCourseId() != 99999) {
+					Course course = user.getCourse(user.getSelectedCourseId());
+					footerCourseHdr.setText(course.getShortName());
+
+					getCourseAssignments();
 				}
 			}
 		}
-		
-		private void getCourseAssignments() {
-			emptyLayout = (LinearLayout)findViewById(R.id.coursework_assignitem_empty);
-			emptyLayout.setVisibility(View.INVISIBLE);
-			courseworkLayout = (FrameLayout)findViewById(R.id.assignlistView);
-			courseworkLayout.setVisibility(View.VISIBLE);
-        	
-			ArrayList<CourseContent> coursecontent = new ArrayList<CourseContent>();
-        	if (user != null && user.getCourse(user.getSelectedCourseId()) != null) {
-        		coursecontent = user.getCourse(user.getSelectedCourseId()).getCourseContent();
-        	}        	
-        	
-        	assignArray = CourseContentsListHelper.getInstance(this).populateCourseAssignments(coursecontent);
-    		
-    		if (assignArray != null && assignArray.length > 0) {
-	    		arrayAdapter = new StandardArrayAdapter(this, assignArray);
-	    		sectionAdapter = new SectionListAdapter(getLayoutInflater(), arrayAdapter);
-	    		listView = (SectionListView)findViewById(getResources().getIdentifier("assign_section_list_view", "id", this.getClass().getPackage().getName()));
-	    		listView.setAdapter(sectionAdapter);
-	    		
-	    		listView.setOnItemClickListener(new OnItemClickListener() {
-
-	    			public void onItemClick(AdapterView<?> parent, View view,
-	    					int position, long id) {
-	    							
-	    				Object obj = sectionAdapter.getItem(position);
-	    				if (obj instanceof SectionListItem) {				
-	    				
-	    					SectionListItem	selectedMap = (SectionListItem)obj;
-	    					@SuppressWarnings("unchecked")
-							//String value = ((HashMap<String, String>)selectedMap.item).get("id");
-	    					String url = ((HashMap<String, String>)selectedMap.item).get("url");
-	    					
-	    					if (!url.startsWith("http://") && !url.startsWith("https://")) 
-	    						   url = "http://" + url; 
-	    					
-	    					Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url)); 
-
-	    					try {  
-	    						// Start the activity  
-	    						startActivity(browserIntent);  
-	    					} catch (ActivityNotFoundException e) {  
-	    						// Raise on activity not found  
-	    						Toast.makeText(getApplicationContext(), "Browser not found.", Toast.LENGTH_SHORT).show();  
-	    					}  
-	    				}
-	    			}
-	    		});	
-    		}
-    		else {    			
-    			emptyLayout.setVisibility(View.VISIBLE);
-    			courseworkLayout.setVisibility(View.INVISIBLE);
-    		}
-		}
-			
-
 	}
+
+	private void getCourseAssignments() {
+		emptyLayout = (LinearLayout) findViewById(R.id.coursework_assignitem_empty);
+		emptyLayout.setVisibility(View.INVISIBLE);
+		courseworkLayout = (FrameLayout) findViewById(R.id.assignlistView);
+		courseworkLayout.setVisibility(View.VISIBLE);
+
+		ArrayList<CourseContent> coursecontent = new ArrayList<CourseContent>();
+		if (user != null && user.getCourse(user.getSelectedCourseId()) != null) {
+			coursecontent = user.getCourse(user.getSelectedCourseId())
+					.getCourseContent();
+		}
+
+		assignArray = CourseContentsListHelper.getInstance(this)
+				.populateCourseAssignments(coursecontent);
+
+		if (assignArray != null && assignArray.length > 0) {
+			arrayAdapter = new StandardArrayAdapter(this, assignArray);
+			sectionAdapter = new SectionListAdapter(getLayoutInflater(),
+					arrayAdapter);
+			listView = (SectionListView) findViewById(getResources()
+					.getIdentifier("assign_section_list_view", "id",
+							this.getClass().getPackage().getName()));
+			listView.setAdapter(sectionAdapter);
+
+			listView.setOnItemClickListener(new OnItemClickListener() {
+
+				public void onItemClick(AdapterView<?> parent, View view,
+						int position, long id) {
+
+					Object obj = sectionAdapter.getItem(position);
+					if (obj instanceof SectionListItem) {
+
+						SectionListItem selectedMap = (SectionListItem) obj;
+						@SuppressWarnings("unchecked")
+						// String value = ((HashMap<String,
+						// String>)selectedMap.item).get("id");
+						String url = ((HashMap<String, String>) selectedMap.item)
+								.get("url");
+
+						if (!url.startsWith("http://")
+								&& !url.startsWith("https://"))
+							url = "http://" + url;
+
+						Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+								Uri.parse(url));
+
+						try {
+							// Start the activity
+							startActivity(browserIntent);
+						} catch (ActivityNotFoundException e) {
+							// Raise on activity not found
+							Toast.makeText(getApplicationContext(),
+									"Browser not found.", Toast.LENGTH_SHORT)
+									.show();
+						}
+					}
+				}
+			});
+		} else {
+			emptyLayout.setVisibility(View.VISIBLE);
+			courseworkLayout.setVisibility(View.INVISIBLE);
+		}
+	}
+
+}
--- src/moodle/android/moodle/Setting.java
+++ src/moodle/android/moodle/Setting.java
@@ -1,22 +1,22 @@
 /**
-*  an Android implementation of REST and XML-RPC access to Moodle 2.2 servers or higher
-*  Copyright (C) 2012  Justin Stevanz, Andrew Kelson and Matthias Peitsch
-*
-*	Contact the.omega.online@gmail.com for further information.
-*
-*   This program is free software: you can redistribute it and/or modify
-*    it under the terms of the GNU General Public License as published by
-*    the Free Software Foundation, either version 3 of the License, or
-*    (at your option) any later version.
-*
-*    This program is distributed in the hope that it will be useful,
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*    GNU General Public License for more details.
-*
-*    You should have received a copy of the GNU General Public License
-*    along with this program.  If not, see <http://www.gnu.org/licenses/>
-*/
+ *  an Android implementation of REST and XML-RPC access to Moodle 2.2 servers or higher
+ *  Copyright (C) 2012  Justin Stevanz, Andrew Kelson and Matthias Peitsch
+ *
+ *	Contact the.omega.online@gmail.com for further information.
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
 
 package moodle.android.moodle;
 
@@ -37,79 +37,76 @@ import android.view.View;
 import android.view.WindowManager;
 
 public class Setting extends Activity {
-	
+
 	Intent intent;
-	
+
 	User user;
 
 	/** Called when the activity is first created. */
-	 @ Override
+	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		try {
-			intent = getIntent();	
+			intent = getIntent();
 
 			user = (User) intent.getParcelableExtra("userObject");
-        	
-			
-			
-			//stretch custom dialog horizontally in android
+
+			// stretch custom dialog horizontally in android
 			LayoutInflater factory = LayoutInflater.from(this);
 			final View EntryView = factory.inflate(R.layout.settings, null);
 
-			
-			AlertDialog.Builder adb = new AlertDialog.Builder(this); 
+			AlertDialog.Builder adb = new AlertDialog.Builder(this);
 			adb.setTitle("Settings");
 			adb.setIcon(R.drawable.setting_icon);
-			adb.setInverseBackgroundForced(true);	
-			adb.setPositiveButton("Logout", new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog, int which) {
-					logout();
-					finish();
-					return;
-				}});
+			adb.setInverseBackgroundForced(true);
+			adb.setPositiveButton("Logout",
+					new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int which) {
+							logout();
+							finish();
+							return;
+						}
+					});
 			adb.setNeutralButton("Save", new DialogInterface.OnClickListener() {
 				public void onClick(DialogInterface dialog, int which) {
-					//Downloading of files process started here....
-					
+					// Downloading of files process started here....
+
 					finish();
 					return;
-				}});	
-			adb.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog, int which) {
-					finish();
-					return;
-				}});	
-			
-			
-			
-		    //Dialog d = adb.setView(new View(this)).create();
-			Dialog d = adb.setView(EntryView).create(); 
-			
-		    WindowManager.LayoutParams lp = new WindowManager.LayoutParams(); 
-		    lp.copyFrom(d.getWindow().getAttributes()); 
-		    lp.width = WindowManager.LayoutParams.FILL_PARENT; 
-		    lp.height = WindowManager.LayoutParams.FILL_PARENT; 
+				}
+			});
+			adb.setNegativeButton("Cancel",
+					new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int which) {
+							finish();
+							return;
+						}
+					});
 
-		    d.show(); 
-		    d.getWindow().setAttributes(lp); 
-		    d.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
-			
+			// Dialog d = adb.setView(new View(this)).create();
+			Dialog d = adb.setView(EntryView).create();
 
-			
-			
-		    
+			WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+			lp.copyFrom(d.getWindow().getAttributes());
+			lp.width = WindowManager.LayoutParams.FILL_PARENT;
+			lp.height = WindowManager.LayoutParams.FILL_PARENT;
+
+			d.show();
+			d.getWindow().setAttributes(lp);
+			d.getWindow().setBackgroundDrawable(
+					new ColorDrawable(android.graphics.Color.TRANSPARENT));
+
 		} catch (Exception e) {
-			Log.e("Error 3", e.toString()+" Error within Settings page");
+			Log.e("Error 3", e.toString() + " Error within Settings page");
 		}
 	}
-	 public void logout(){
-		Intent nextPage;
- 		nextPage = new Intent(Setting.this, Login.class);
- 		nextPage.putExtra("userObject", user);
-     	startActivity(nextPage);
-	 }
 
-		
+	public void logout() {
+		Intent nextPage;
+		nextPage = new Intent(Setting.this, MainApp.class);
+		nextPage.putExtra("userObject", user);
+		startActivity(nextPage);
+	}
+
 }
